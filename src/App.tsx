6384adff771250a6a5b89fa5c925552e8fdb365f
Vitor Silva <developer.vitorsilva@gmail.com>
2021-05-22 10:19:19 -0300

    created first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Page from './components/Page';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <GlobalStyles />
+      <Page />
+    </>
   );
 }
 
---
+++ src/components/Page/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import DefaultOverlayContent from '../DefaultOverlayContent';
+import { ModelSection, ModelsWrapper } from '../Model';
+
+import { Container } from './styles';
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          <ModelSection modelName="Model One"
+          overlayNode={
+            <DefaultOverlayContent
+              label="Model One"
+              description="Order Online for Delivery"
+            />
+          } />
+        </div>
+      </ModelsWrapper>
+    </Container>
+  );
+};
+
+export default Page;
