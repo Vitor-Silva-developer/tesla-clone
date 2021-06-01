e351423829362ece04baf3e808f832744da5b4a6
Vitor Silva <developer.vitorsilva@gmail.com>
2021-06-01 17:02:11 -0300

    made a map on models sections

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import DefaultOverlayContent from '../DefaultOverlayContent';
-import { ModelSection, ModelsWrapper } from '../Model';
+import React from "react";
+import DefaultOverlayContent from "../DefaultOverlayContent";
+import { ModelSection, ModelsWrapper } from "../Model";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const Page: React.FC = () => {
   return (
     <Container>
       <ModelsWrapper>
         <div>
-          <ModelSection modelName="Model One"
-          overlayNode={
-            <DefaultOverlayContent
-              label="Model One"
-              description="Order Online for Delivery"
+          {[
+            'Model S',
+            'Model Y',
+            'Model 3',
+            'Model X',
+            'Solar Panels',
+            'Solar Roof',
+            'Acessories',
+          ].map(modelName => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
             />
-          } />
+          ))}
         </div>
       </ModelsWrapper>
     </Container>
