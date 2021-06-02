2268f7f3eb47c9f949794187d5d9a921364912d3
Vitor Silva <developer.vitorsilva@gmail.com>
2021-06-02 14:31:34 -0300

    added images

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -2,25 +2,39 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   .colored:nth-child(1) {
-    background: #f1ffe7;
+    background-image: url('images/ms-homepage-desktop.jpeg');
+    background-size: cover;
+    background-position: 0 -260px;
   }
   .colored:nth-child(2) {
-    background: #dfffd9;
+    background-image: url('images/Desktop-ModelY.jpeg');
+    background-size: cover;
+    background-position: 0 -260px;
   }
   .colored:nth-child(3) {
-    background: #cdfeca;
+    background-image: url('images/m3-homepage-desktop.jpeg');
+    background-size: cover;
+    background-position: 0 -260px;
   }
   .colored:nth-child(4) {
-    background: #bbfebb;
+    background-image: url('images/mx-homepage-desktop.jpeg');
+    background-size: cover;
+    background-position: 0 -260px;
   }
   .colored:nth-child(5) {
-    background: #a9fdac;
+    background-image: url('images/us_homepage_sp_hero_d.jpeg');
+    background-size: cover;
+    background-position: 0 -100px;
   }
   .colored:nth-child(6) {
-    background: #90f29c;
+    background-image: url('images/solar-roof-hero-desktop.jpeg');
+    background-size: cover;
+    background-position: 0 -50px;
   }
   .colored:nth-child(7) {
-    background: #77e68c;
+    background-image: url('images/Desktop-Accessories.jpeg');
+    background-size: cover;
+    background-position: 0 -250px;
   }
 `;
 
